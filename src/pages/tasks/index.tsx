ff147fb74d1288f445a8841c046eb901b51b27ef
hori <fourbasic19@gmail.com>
2023-05-02 16:10:21 +0900

    【tsx】ポップアップ表示設置

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import TasksSection from "../../components/TasksSection";
+import PopUp from "../../components/PopUp";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
+      <PopUp />
       <TasksSection />
     </main>
   );
