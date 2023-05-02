61c55630a65ae0aa862e8a2cdf6136f3a2ec6f60
hori <fourbasic19@gmail.com>
2023-05-02 17:42:43 +0900

    【jsx】新規作成が正しく完了したときに、全タスクを一覧を更新する。

--- src/components/TaskCreate.tsx
+++ src/components/TaskCreate.tsx
@@ -2,52 +2,63 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
-const TaskCreate = () => {
+interface TaskCreateProps {
+  fetchTasks: () => void;
+}
+
+const TaskCreate = ({ fetchTasks }: TaskCreateProps) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: any, e: any) => {
     try {
       await axios.post("https://back-mongo-task.vercel.app/api/v1/tasks", data);
-      alert("タスクが追加されました");
+      fetchTasks(); // タスクリストを更新する
+      e.target.reset(); // フォームの値をリセットする
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="bg-blue-50 p-4 rounded-lg">
+    <div>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="border border-gray-300 rounded-lg p-4 flex flex-row"
+        className="mx-8 bg-blue-50 border border-gray-300 rounded-lg p-4 flex flex-row items-center"
       >
-        <label className="mr-4">
-          Name:
+        <label className="mr-4 flex items-center h-full">
+          <span>Name:</span>
           <input
             {...register("name", {
               required: true,
               maxLength: 20,
             })}
-            className="ml-2 border rounded p-2"
+            className="ml-2 border rounded p-2 h-full"
           />
           {errors.name && errors.name.type === "required" && (
-            <span>名前は必須です</span>
+            <span className="text-red-500 ml-2">名前は必須です</span>
           )}
           {errors.name && errors.name.type === "maxLength" && (
-            <span>名前は20文字以内である必要があります</span>
+            <span className="text-red-500 ml-2">
+              名前は20文字以内である必要があります
+            </span>
           )}
         </label>
-        <label>
-          Completed:
-          <input {...register("completed")} type="checkbox" className="ml-2" />
+        <label className="flex items-center h-full">
+          <input
+            {...register("completed")}
+            type="checkbox"
+            className="ml-4 h-5 w-5"
+          />
+          <span className="ml-2">Completed</span>
         </label>
         <input
           type="submit"
           value="追加"
-          className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+          className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         />
       </form>
     </div>
--- src/components/TasksSection.tsx
+++ src/components/TasksSection.tsx
@@ -31,8 +31,8 @@ const TasksSection = () => {
 
   return (
     <div className="container mx-auto mt-5">
-      <h1 className="text-2xl font-bold mb-5">Tasks</h1>
-      <TaskCreate />
+      <h1 className="text-2xl font-bold mb-5 text-center">Tasks</h1>
+      <TaskCreate fetchTasks={fetchTasks} />
       <table className="table-auto w-full">
         <thead>
           <tr>
