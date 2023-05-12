75e4cba627cc18d595d86e51741dc97a8c126764
hori <fourbasic19@gmail.com>
2023-05-12 13:32:37 +0900

    【バグフィックス】modal設定を_app.jsに記述

--- src/components/Modalpop.tsx
+++ src/components/Modalpop.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import ModalContent from "../components/ModalContent";
 
+Modal.setAppElement("#__next");
+
 const Modalpop = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -15,8 +17,14 @@ const Modalpop = () => {
 
   return (
     <div>
-      <button onClick={openModal}>Open Modal</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+        onClick={openModal}
+      >
+        Edit
+      </button>
       <Modal
+        id={"ddddddeeeeeee"}
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full sm:w-3/5"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import ReactModal from "react-modal";
 
 export default function App({ Component, pageProps }: AppProps) {
+  ReactModal.setAppElement("#__next");
+
   return <Component {...pageProps} />
 }
