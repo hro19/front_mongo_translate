87f3489eb36e14c15c19fac75ada778af6072864
hori <fourbasic19@gmail.com>
2023-05-16 14:50:46 +0900

    【検索機能】カテゴリーをいれて、複合検索

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,59 +1,97 @@
-// 参考記事：Reactでリアルタイムの検索機能を実装する　https://qiita.com/hinako_n/items/a0745afe0631578c698e
 import React, { useState } from 'react';
 
 const posts = [
   {
     title: "useStateの使い方",
-    category: "React",
+    category: ["React", "JavaScript", "TypeScript"],
   },
   {
     title: "LaravelのMVCモデルについて",
-    category: "Laravel",
+    category: ["React", "Laravel"],
   },
   {
     title: "同一オリジンポリシーとCORS",
-    category: "Web",
+    category: ["Web"],
   },
   {
     title: "useEffectの使い方",
-    category: "React",
+    category: ["Laravel"],
   },
 ];
 
 const search = () => {
   const [showPosts, setShowPosts] = useState(posts);
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState(["All"]);
 
   // 検索欄への入力値をハンドリング
-  const handleInputChange = (e:any) => {
-    setInputValue(e.target.value);
-    search(e.target.value);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    search(selectedCategories, value);
   };
 
-  // 検索欄への入力値での絞り込み
-  const search = (value: any) => {
-    // 検索欄への入力が空の場合は早期return
-    if (value === "") {
+  // カテゴリーの選択をハンドリング
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOptions = Array.from(e.target.selectedOptions);
+    const selectedValues = selectedOptions.map((option) => option.value);
+    setSelectedCategories(selectedValues);
+    search(selectedValues, inputValue);
+  };
+
+  // カテゴリーと入力値での絞り込み
+  const search = (categories: string[], value: string) => {
+    // カテゴリーが "All" で入力値が空の場合は全ての記事を表示
+    if (categories.includes("All") && value === "") {
       setShowPosts(posts);
       return;
     }
 
-    const serchedPosts = posts.filter(
-      (post) =>
-        Object.values(post).filter(
-          (item) =>
-            item !== undefined &&
-            item !== null &&
-            item.toUpperCase().indexOf(value.toUpperCase()) !== -1
-        ).length > 0
-    );
+    const filteredPosts = posts.filter((post) => {
+      if (categories.includes("All")) {
+        // カテゴリーが "All" の場合、入力値でのみ絞り込み
+        return (
+          post.title.toLowerCase().includes(value.toLowerCase()) ||
+          post.category.some((cat) =>
+            cat.toLowerCase().includes(value.toLowerCase())
+          )
+        );
+      } else {
+        // カテゴリーが選択されている場合、カテゴリーと入力値の両方で絞り込み
+        return (
+          categories.some((cat) => post.category.includes(cat)) &&
+          (post.title.toLowerCase().includes(value.toLowerCase()) ||
+            post.category.some((cat) =>
+              cat.toLowerCase().includes(value.toLowerCase())
+            ))
+        );
+      }
+    });
 
-    setShowPosts(serchedPosts);
+    setShowPosts(filteredPosts);
   };
 
   return (
     <div className="App">
       <h1>記事一覧</h1>
+
+      {/* カテゴリー選択フォーム */}
+      <div>
+        <h4>カテゴリー</h4>
+        <select
+          value={selectedCategories}
+          onChange={handleCategoryChange}
+          multiple
+          className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          <option value="All">All</option>
+          <option value="React">React</option>
+          <option value="Laravel">Laravel</option>
+          <option value="Web">Web</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="TypeScript">TypeScript</option>
+        </select>
+      </div>
 
       {/* フリーキーワード検索フォーム */}
       <div>
@@ -65,7 +103,6 @@ const search = () => {
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
-
       {/* 記事一覧表示 */}
       {showPosts.map((post, index) => {
         return (
@@ -73,12 +110,12 @@ const search = () => {
             <p>
               {index + 1}. {post.title}
             </p>
-            <p>category：{post.category}</p>
+            <p>category：{post.category.join(", ")}</p>
           </div>
         );
       })}
     </div>
   );
-}
+};
 
-export default search
+export default search;
