0be5ffc412d8bf0b3873289d640ff4ed47e7f973
hori <fourbasic19@gmail.com>
2023-05-16 13:47:02 +0900

    【tsx】イテレート箇所をコンポーネント化

---
+++ src/components/translates/TranslateItelete.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import TranslateDelete from "../../components/translates/TranslateDelete";
+import { formatDate } from "../../components/translates/Honyaku";
+
+const TranslateItelete = ({
+  translate,
+  isSpeaking,
+  stopSpeaking,
+  speakText,
+}: any) => {
+  return (
+    <>
+      <div key={translate._id} className="border-b border-bp mb-2 pb-2 b-4">
+        <h2 className="text-sm font-bold text-green-700">
+          作成日【{formatDate(translate.created_at)}】
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <p className="border rounded-lg p-2 bg-blue-100 text-left">
+              {translate.jaContent}
+            </p>
+            <TranslateDelete translate={translate} />
+          </div>
+          <div>
+            <p className="border rounded-lg p-2 bg-pink-100 text-left">
+              {translate.enContent}
+            </p>
+            {isSpeaking ? (
+              <button
+                className="mt-2 bg-teal-500 text-white py-1 px-2 rounded-md hover:bg-teal-700 text-sm"
+                onClick={stopSpeaking}
+              >
+                再生ストップ
+              </button>
+            ) : (
+              <button
+                className="mt-2 bg-cyan-500 text-white py-1 px-2 rounded-md hover:bg-cyan-700 text-sm"
+                onClick={() => speakText(translate.enContent)}
+              >
+                音声データ
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TranslateItelete
--- src/pages/translates/all.tsx
+++ src/pages/translates/all.tsx
@@ -3,11 +3,9 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import ScaleLoader from "react-spinners/ScaleLoader";
 
-import TranslateDelete from "../../components/translates/TranslateDelete";
+import TranslateItelete from "../../components/translates/TranslateItelete";
 import TranslateTitle from "../../components/translates/TranslateTitle";
-import {
-  formatDate
-} from "../../components/translates/Honyaku";
+
 
 const All = () => {
   const fetchTranslates = async () => {
@@ -56,42 +54,7 @@ const All = () => {
           <div>Error: {(error as Error).message}</div>
         ) : (
           data.map((translate: any, index: number) => (
-            <div
-              key={translate._id}
-              className="border-b border-bp mb-2 pb-2 b-4"
-            >
-              <h2 className="text-sm font-bold text-green-700">
-                作成日【{formatDate(translate.created_at)}】
-              </h2>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                  <p className="border rounded-lg p-2 bg-blue-100 text-left">
-                    {translate.jaContent}
-                  </p>
-                  <TranslateDelete translate={translate} />
-                </div>
-                <div>
-                  <p className="border rounded-lg p-2 bg-pink-100 text-left">
-                    {translate.enContent}
-                  </p>
-                  {isSpeaking ? (
-                    <button
-                      className="mt-2 bg-teal-500 text-white py-1 px-2 rounded-md hover:bg-teal-700 text-sm"
-                      onClick={stopSpeaking}
-                    >
-                      再生ストップ
-                    </button>
-                  ) : (
-                    <button
-                      className="mt-2 bg-cyan-500 text-white py-1 px-2 rounded-md hover:bg-cyan-700 text-sm"
-                      onClick={() => speakText(translate.enContent)}
-                    >
-                      音声データ
-                    </button>
-                  )}
-                </div>
-              </div>
-            </div>
+            <TranslateItelete key={translate._id} translate={translate} isSpeaking={isSpeaking} stopSpeaking={stopSpeaking} speakText={speakText} />
           ))
         )}
       </div>
