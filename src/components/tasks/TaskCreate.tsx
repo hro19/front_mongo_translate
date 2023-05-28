bdf4a93c5d9ad9ad671f5e4b83f0329da0732528
hori <fourbasic19@gmail.com>
2023-05-28 14:55:42 +0900

    【react-hook-form】エラー文の設定の書き方を変更

--- src/components/tasks/TaskCreate.tsx
+++ src/components/tasks/TaskCreate.tsx
@@ -42,18 +42,19 @@ const TaskCreate = ({ refetch }: TaskCreateProps) => {
           <span>Name:</span>
           <input
             {...register("name", {
-              required: true,
-              maxLength: 20,
+              required: {
+                value: true,
+                message: "名前は必須です",
+              },
+              maxLength: {
+                value: 20,
+                message: "名前は20文字以内である必要があります",
+              },
             })}
             className="ml-2 border rounded p-2 h-full"
           />
-          {errors.name && errors.name.type === "required" && (
-            <span className="text-red-500 ml-2">名前は必須です</span>
-          )}
-          {errors.name && errors.name.type === "maxLength" && (
-            <span className="text-red-500 ml-2">
-              名前は20文字以内である必要があります
-            </span>
+          {errors.name && (
+            <span className="text-red-500 ml-2">{errors.name.message}</span>
           )}
         </label>
         <label className="flex items-center h-full">
@@ -67,7 +68,7 @@ const TaskCreate = ({ refetch }: TaskCreateProps) => {
         <input
           type="submit"
           value="追加"
-          className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="ml-4 bg-emerald-700 hover:bg-emerald-600 cursor-pointer text-white font-bold py-2 px-4 rounded"
         />
       </form>
     </div>
