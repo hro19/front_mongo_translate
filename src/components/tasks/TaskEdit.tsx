7a57cbc46c1255029fc3d6859011db11f4f18e0c
hori <fourbasic19@gmail.com>
2023-05-31 01:19:44 +0900

    【tsx】不要な記述を削除

--- src/components/tasks/TaskEdit.tsx
+++ src/components/tasks/TaskEdit.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from "axios";
-import { Task, TaskIterateObj } from "../../ts/Task";
+import { Task } from "../../ts/Task";
 
 interface TaskDeleteProps {
   task: Task;
