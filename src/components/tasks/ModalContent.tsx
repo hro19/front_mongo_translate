e26efe258a7d9cedad32b435c320a7801b5d9ed3
hori <fourbasic19@gmail.com>
2023-05-30 20:51:20 +0900

    【tsx】フォームをコンポーネント化

--- src/components/tasks/ModalContent.tsx
+++ src/components/tasks/ModalContent.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Task, TaskObj } from "../../ts/Task";
 import { useMutation, useQueryClient } from "react-query";
+import ModalContentForm from "./ModalContentForm";
 
 type ModalContentProps = TaskObj & {
   closeModal: () => void;
@@ -28,10 +29,6 @@ const ModalContent = ({ task, closeModal }: ModalContentProps) => {
     }
   );
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCompleted(e.target.value === "completed");
-  };
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -51,6 +48,7 @@ const ModalContent = ({ task, closeModal }: ModalContentProps) => {
       // エラーが発生した場合は、適切なエラーハンドリングを行う
     }
   };
+
   return (
     <div className="bg-slate-500">
       <div
@@ -61,65 +59,14 @@ const ModalContent = ({ task, closeModal }: ModalContentProps) => {
           className="w-full sm:w-4/5 lg:w-3/4 max-w-md"
           onSubmit={handleSubmit}
         >
-          <h3 className="text-lg font-bold text-center mb-4">
-            【ID】{task._id}
-          </h3>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="name"
-            >
-              名前
-            </label>
-            <input
-              className="appearance-none border rounded w-full py-2 px-3 text-2xl text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
-              type="text"
-              placeholder="名前を入力してください"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <span className="block text-gray-700 font-bold mb-2">進捗</span>
-            <div className="flex items-center">
-              <input
-                className="mr-2"
-                type="radio"
-                id="inprogress"
-                name="progress"
-                value="inprogress"
-                checked={!completed}
-                onChange={handleChange}
-              />
-              <label htmlFor="inprogress">進行中</label>
-            </div>
-            <div className="flex items-center">
-              <input
-                className="mr-2"
-                type="radio"
-                id="completed"
-                name="progress"
-                value="completed"
-                checked={completed}
-                onChange={handleChange}
-              />
-              <label htmlFor="completed">完了</label>
-            </div>
-          </div>
-          <button
-            type="button"
-            className="bg-gray-500 text-white py-2 px-4 rounded mr-3"
-            onClick={() => closeModal()}
-          >
-            キャンセル
-          </button>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded"
-          >
-            更新する
-          </button>
+          <ModalContentForm
+            name={name}
+            setName={setName}
+            completed={completed}
+            setCompleted={setCompleted}
+            task={task}
+            closeModal={closeModal}
+          />
         </form>
       </div>
     </div>
