f164d50f9ed703abfbe35a222f910cbb99b17527
hori <fourbasic19@gmail.com>
2023-06-02 19:48:59 +0900

    【Ts】ユニオン型

--- src/pages/tss/tsone.tsx
+++ src/pages/tss/tsone.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
 
 const TsOne = () => {
-
   const obj = { a: 1, b: 2, c: 3 };
   const keys2 = Object.keys(obj);
 
+  //typeof関数
   const point = { x: 135, y: 35 };
   type Point = typeof point;
 
-  type Person = {
-    name: string;
-  };
-
+  //リテラル型
   let only1: 1;
   only1 = 1;
+
+  //ユニオン型
+  let numberOrString: number | string;
+  numberOrString = 3;
+  console.log(numberOrString);
+  numberOrString = "小早川";
+  console.log(numberOrString);
+
 
   return (
     <>
       <ul>
-        <li>リンゴ</li>
-        <li>
-          <h2>typeof型演算子</h2>
-          <p>
-            TypeScriptのtypeofは変数から型を抽出する型演算子です。
-            <br />
-            例:変数pointにtypeof型演算子を用いて、Point型を定義できます。
-          </p>
-        </li>
-        <li>ぶどう</li>
+        <li>typeof型演算子</li>
+        <li>リテラル型で特定の値だけを格納できるようにする</li>
+        <li>ユニオン型で一つの変数に複数の型を持たせることができるようにする</li>
+        <li>typeof型演算子</li>
       </ul>
     </>
   );
