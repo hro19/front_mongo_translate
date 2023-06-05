6ce7f607a23a352fd15cf5c1c21f987d7d86bb95
hori <fourbasic19@gmail.com>
2023-06-05 17:16:27 +0900

    【react】TaskSectionコンポーネントを削除

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 import { Inter } from "next/font/google";
-import TaskCreate from "../../components/tasks/TaskCreate";
-import TasksSection from "../../components/tasks/TasksSection";
 import { useAtom } from "jotai";
 import { isSnakeAtom } from "../../jotai/atoms";
+import TaskCreate from "../../components/tasks/TaskCreate";
+import TaskTable from "../../components/tasks/TaskTable";
 import SnakeMessage from "../../components/taskShingle/SnakeMessage";
+import { Task } from "../../ts/Task";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +15,26 @@ export default function Home() {
   //popoverメッセージを制御する
   const [isSnake, setIsSnake] = useAtom(isSnakeAtom);
 
+  //すべてのタスクを取得＆react-queryでキャッシュ管理
+  const fetchTasks = async () => {
+    const { data } = await axios.get(
+      "https://back-mongo-task2.vercel.app/api/v1/tasks"
+    );
+    return data;
+  };
+
+  const { data, isLoading, isError } = useQuery<Task[]>("tasks", fetchTasks);
+
+  if (isError) {
+    return <div>Error fetching tasks.</div>;
+  }
+
   return (
     <main>
       <div className="container mx-auto mt-5">
         <h1 className="text-2xl font-bold my-5 text-center">Tasks</h1>
         <TaskCreate />
-        <TasksSection />
+        <TaskTable tasks={data} isLoading={isLoading} />
         {isSnake && <SnakeMessage />}
       </div>
     </main>
