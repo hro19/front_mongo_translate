1cced232b5d5203d1169484a7542abd2e3dcbe43
hori <fourbasic19@gmail.com>
2023-06-09 17:52:44 +0900

    【Jotai】型定義

--- src/jotai/atoms.tsx
+++ src/jotai/atoms.tsx
@@ -1,16 +1,21 @@
-import { atom } from 'jotai';
-import { SelectSwitch } from "../ts/Task";
+import { atom } from "jotai";
+import { SelectSwitch, Task } from "../ts/Task";
 
 //tasks一覧ページ
 export const tasksStateAtom = atom<SelectSwitch>("all");
 
 //tasksシングルページ
-export const isSnakeAtom = atom(false);
-export const snakeDurationAtom = atom(2000);
-export const checkEditAtom = atom(false);
+export const isSnakeAtom = atom<boolean>(false);
+export const snakeDurationAtom = atom<number>(2000);
+export const checkEditAtom = atom<boolean>(false);
 
-export const nameAtom = atom("");
-export const jaNameAtom = atom("");
-export const completedAtom = atom(false);
+export const nameAtom = atom<string>("");
+export const jaNameAtom = atom<string>("");
+export const completedAtom = atom<boolean>(false);
 
-export const taskAtom = atom({ _id: "", name: "",jaName:"", completed: false });
+export const taskAtom = atom<Task>({
+  _id: "",
+  name: "",
+  jaName: "",
+  completed: false,
+});
