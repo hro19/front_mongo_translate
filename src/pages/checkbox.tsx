2cbd1cd89c1bf7aa8fb2b369f02b7d57991c3145
hori <fourbasic19@gmail.com>
2023-06-09 22:52:34 +0900

    【react】繰り返し部分を配列にする

--- src/pages/checkbox.tsx
+++ src/pages/checkbox.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+type Hinshi = {
+  value: string;
+  label: string;
+};
+
 type FormData = {
   checkboxes: string[];
 };
 
 const CheckboxForm = () => {
+  const hinshi: Hinshi[] = [
+    { value: "verb", label: "動詞" },
+    { value: "adjective", label: "形容詞" },
+    { value: "adverb", label: "副詞" },
+    { value: "auxiliary", label: "助動詞" },
+  ];
+
   const {
     register,
     handleSubmit,
@@ -19,46 +31,18 @@ const CheckboxForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label>
-          <input
-            type="checkbox"
-            value="動詞"
-            {...register("checkboxes", {
-              required: "少なくとも1つの項目を選択してください",
-            })}
-          />
-          動詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            value="形容詞"
-            {...register("checkboxes", {
-              required: "少なくとも1つの項目を選択してください",
-            })}
-          />
-          形容詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            value="副詞"
-            {...register("checkboxes", {
-              required: "少なくとも1つの項目を選択してください",
-            })}
-          />
-          副詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            value="助動詞"
-            {...register("checkboxes", {
-              required: "少なくとも1つの項目を選択してください",
-            })}
-          />
-          助動詞
-        </label>
+        {hinshi.map((checkbox) => (
+          <label key={checkbox.value}>
+            <input
+              type="checkbox"
+              value={checkbox.value}
+              {...register("checkboxes", {
+                required: "少なくとも1つの項目を選択してください",
+              })}
+            />
+            {checkbox.label}
+          </label>
+        ))}
       </div>
       {errors.checkboxes && (
         <span className="text-red-500">{errors.checkboxes.message}</span>
