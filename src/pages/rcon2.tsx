08c57b8e8d42c3a01428a0ea9c9843d23a1c5098
hori <fourbasic19@gmail.com>
2023-06-16 23:40:09 +0900

    【typescript】css調整

--- src/pages/rcon2.tsx
+++ src/pages/rcon2.tsx
@@ -27,12 +27,21 @@ const Rcon2 = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="text">テキスト</label>
+        <label htmlFor="text" className="block text-gray-700">
+          Text
+        </label>
         <Controller
           name="text"
           control={control}
           rules={{ required: true }}
-          render={({ field }) => <input {...field} type="text" id="text" />}
+          render={({ field }) => (
+            <input
+              {...field}
+              type="text"
+              id="text"
+              className="block w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          )}
         />
         {errors.text && <p>テキストの入力は必須です。</p>}
       </div>
@@ -56,7 +65,12 @@ const Rcon2 = () => {
         />
         {errors.speech && <p>少なくとも1つの選択が必要です。</p>}
       </div>
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="mt-2 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+      >
+        Submit
+      </button>
     </form>
   );
 };
