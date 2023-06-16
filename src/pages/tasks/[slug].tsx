274844602ea4a6c3f149293100a4b180ba24510f
hori <fourbasic19@gmail.com>
2023-06-17 02:21:57 +0900

    【react】react-queryとfetchデータの基本

--- src/pages/tasks/[slug].tsx
+++ src/pages/tasks/[slug].tsx
@@ -1,65 +1,54 @@
-import React, { useState,useEffect } from "react";
-import { GetServerSideProps } from "next";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
 import axios from "axios";
-import Link from "next/link";
-import { useAtom } from "jotai";
-import { isSnakeAtom, taskAtom } from "../../jotai/atoms";
-import SlugForm from "../../components/taskShingle/SlugForm";
-import SnakeMessage from "../../components/taskShingle/SnakeMessage";
-import { CurrentTaskObj } from "../../ts/Task";
+import { useRouter } from "next/router";
 
-export const getServerSideProps: GetServerSideProps<CurrentTaskObj> = async (
-  context
-) => {
-  const { slug } = context.query;
-
-  try {
-    const response = await axios.get(
-      `https://back-mongo-task2.vercel.app/api/v1/tasks/${slug}`
-    );
-    const currentTask = response.data;
-
-    return {
-      props: {
-        currentTask,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching single task:", error);
-    return {
-      props: {
-        currentTask: null,
-      },
-    };
-  }
+const fetchTask = async (slug: string) => {
+  const response = await axios.get(
+    `https://back-mongo-task2.vercel.app/api/v1/tasks/${slug}`
+  );
+  return response.data;
 };
 
-const SingleTaskPage = ({ currentTask }: CurrentTaskObj) => {
-  const [task, setTask] = useAtom(taskAtom);
+const SingleTask = () => {
+  const router = useRouter();
+  const { slug } = router.query;
 
-  //currentTaskをTaskとしてセットする
+  const {
+    data: task,
+    isLoading,
+    error,
+  } = useQuery(["task", slug], () => fetchTask(slug as string), {
+    enabled: !!slug, // もし slug が存在する場合にのみクエリを実行する
+  });
+
   useEffect(() => {
-    setTask(currentTask);
-  }, []);
+    if (slug) {
+      // Prefetch the task data
+      fetchTask(slug as string);
+    }
+  }, [slug]);
 
-  //popoverメッセージを制御する
-  const [isSnake, setIsSnake] = useAtom(isSnakeAtom);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching task: </div>;
+    //   {error.message;}
+  }
+
+  if (!task) {
+    return null; // Handle the case when the task is not available
+  }
 
   return (
-    <>
-      <div>
-        <h1>【ID】{task._id}</h1>
-        <p>【英単語】{task.name}</p>
-        <p>【日本語訳】{task.jaName}</p>
-        <p>【進行具合】{task.completed ? "暗記済み" : "勉強中"}</p>
-      </div>
-      <SlugForm />
-      {isSnake && <SnakeMessage />}
-      <Link href="/tasks/" className="text-4xl mt-6">
-        トップに戻る
-      </Link>
-    </>
+    <div>
+      <h1>{task.name}</h1>
+      <p>{task._id}</p>
+      <p>{task.completed ? "Completed" : "Not Completed"}</p>
+    </div>
   );
 };
 
-export default SingleTaskPage;
+export default SingleTask;
