a13f8dca88a01f6368aaf8cd927bd78281073d8c
hori <fourbasic19@gmail.com>
2023-06-17 13:44:30 +0900

    【rhf】inputの記述を子コンポーネントに書く

--- src/components/taskShingle/SlugForm.tsx
+++ src/components/taskShingle/SlugForm.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useAtom } from "jotai";
 import { isSnakeAtom } from "../../jotai/atoms";
 import { useMutation, useQueryClient } from "react-query";
+import SlugFormInput from "./SlugFormInput";
 
 const SlugForm = ({ task,slug }: any) => {
   const queryClient = useQueryClient();
@@ -50,87 +51,7 @@ const SlugForm = ({ task,slug }: any) => {
   return (
     <div className="bg-slate-300">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="name">名前</label>
-          <Controller
-            control={control}
-            name="name"
-            defaultValue={task.name}
-            rules={{
-              required: "名前は必須です",
-              maxLength: {
-                value: 10,
-                message: "名前は10文字以下で入力してください",
-              },
-            }}
-            render={({ field }) => (
-              <input
-                {...field}
-                id="name"
-                type="text"
-                placeholder="名前を入力してください"
-                className={`form-control ${errors.name ? "is-invalid" : ""}`}
-              />
-            )}
-          />
-          {errors.name && (
-            <span className="text-danger">
-              {errors.name.message as React.ReactNode}
-            </span>
-          )}
-        </div>
-        <div>
-          <label htmlFor="jaName">日本語訳</label>
-          <Controller
-            control={control}
-            name="jaName"
-            defaultValue={task.jaName}
-            rules={{
-              required: "日本語訳は必須です",
-              maxLength: {
-                value: 20,
-                message: "日本語訳は20文字以下で入力してください",
-              },
-            }}
-            render={({ field }) => (
-              <input
-                {...field}
-                id="jaName"
-                type="text"
-                placeholder="日本語訳を入力してください"
-                className={`form-control ${errors.jaName ? "is-invalid" : ""}`}
-              />
-            )}
-          />
-          {errors.jaName && (
-            <span className="text-danger">
-              {errors.jaName.message as React.ReactNode}
-            </span>
-          )}
-        </div>
-        <div>
-          <label htmlFor="completed">
-            <Controller
-              control={control}
-              name="completed"
-              defaultValue={task.completed}
-              render={({ field }) => (
-                <input
-                  {...field}
-                  id="completed"
-                  type="checkbox"
-                  className="mr-1"
-                  checked={field.value} // フォームの値に応じてチェックを設定
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    field.onChange(e.target.checked)
-                  } // チェックボックスの状態をフォームの値に反映
-                />
-              )}
-            />
-            暗記済み
-          </label>
-        </div>
-        <button type="submit">送信</button>
+        <SlugFormInput task={task} control={control} errors={errors} />
       </form>
     </div>
   );
--- src/components/taskShingle/SlugFormInput.tsx
+++ src/components/taskShingle/SlugFormInput.tsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React from 'react';
+import { Controller } from "react-hook-form";
 
-const SlugFormInput = () => {
+const SlugFormInput = ({task,control,errors}:any) => {
   return (
-    <div>
-      zzzzz
-    </div>
-  )
+    <>
+      <div>
+        <label htmlFor="name">名前</label>
+        <Controller
+          control={control}
+          name="name"
+          defaultValue={task.name}
+          rules={{
+            required: "名前は必須です",
+            maxLength: {
+              value: 10,
+              message: "名前は10文字以下で入力してください",
+            },
+          }}
+          render={({ field }) => (
+            <input
+              {...field}
+              id="name"
+              type="text"
+              placeholder="名前を入力してください"
+              className={`form-control ${errors.name ? "is-invalid" : ""}`}
+            />
+          )}
+        />
+        {errors.name && (
+          <span className="text-danger">
+            {errors.name.message as React.ReactNode}
+          </span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="jaName">日本語訳</label>
+        <Controller
+          control={control}
+          name="jaName"
+          defaultValue={task.jaName}
+          rules={{
+            required: "日本語訳は必須です",
+            maxLength: {
+              value: 20,
+              message: "日本語訳は20文字以下で入力してください",
+            },
+          }}
+          render={({ field }) => (
+            <input
+              {...field}
+              id="jaName"
+              type="text"
+              placeholder="日本語訳を入力してください"
+              className={`form-control ${errors.jaName ? "is-invalid" : ""}`}
+            />
+          )}
+        />
+        {errors.jaName && (
+          <span className="text-danger">
+            {errors.jaName.message as React.ReactNode}
+          </span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="completed">
+          <Controller
+            control={control}
+            name="completed"
+            defaultValue={task.completed}
+            render={({ field }) => (
+              <input
+                {...field}
+                id="completed"
+                type="checkbox"
+                className="mr-1"
+                checked={field.value} // フォームの値に応じてチェックを設定
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  field.onChange(e.target.checked)
+                } // チェックボックスの状態をフォームの値に反映
+              />
+            )}
+          />
+          暗記済み
+        </label>
+      </div>
+      <button type="submit">送信</button>
+    </>
+  );
 }
 
 export default SlugFormInput
