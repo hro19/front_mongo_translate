a3cd860102ed08e7ee7a57dcc33008c0fb1ef7eb
hori <fourbasic19@gmail.com>
2023-06-18 19:45:44 +0900

    【daisyUI】ツールチップをdaisyに置換

--- src/components/translates/Tooltips.tsx
+++ src/components/translates/Tooltips.tsx
@@ -1,35 +1,34 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import Fade from "@mui/material/Fade";
-import Zoom from "@mui/material/Zoom";
 
 const Tooltips = () => {
+  const tooltipData = [
+    {
+      btnText: "英語/日本語どちらも翻訳可能",
+      explain:
+        "英語/日本語どちらも翻訳可能です、目的とする文章を「コチラに日本語もしくは英語を～」に入力もしくはペーストして翻訳をしてください",
+    },
+    {
+      btnText: "翻訳テキストの保存機能",
+      explain:
+        "「登録」から翻訳データを保存することが出来ます、過去の履歴を振り返れます",
+    },
+    {
+      btnText: "リスニング機能",
+      explain: "翻訳されたデータの英語文をリスニング出来ます",
+    },
+  ];
+
   return (
     <div className="mb-4">
-      {/* <h2 className="mb-3 border-b-2 border-yellow-500 text-yellow-600 text-xl font-bold">使い方</h2> */}
-      <Tooltip title="英語/日本語どちらも翻訳可能です、目的とする文章を「コチラに日本語もしくは英語を～」に入力もしくはペーストして翻訳をしてください">
-        <Button className="px-4 py-1 mr-1 rounded-full border border-navy text-white bg-cyan-900 text-navy hover:bg-cyan-700">
-          英語/日本語どちらも翻訳可能
-        </Button>
-      </Tooltip>
-      <Tooltip
-        TransitionComponent={Fade}
-        TransitionProps={{ timeout: 600 }}
-        title="「登録」から翻訳データを保存することが出来ます、過去の履歴を振り返れます"
-      >
-        <Button className="px-4 py-1 mr-1 rounded-full border border-navy text-white bg-cyan-900 text-navy hover:bg-cyan-700">
-          翻訳テキストの保存機能
-        </Button>
-      </Tooltip>
-      <Tooltip
-        TransitionComponent={Zoom}
-        title="翻訳されたデータの英語文をリスニング出来ます"
-      >
-        <Button className="px-4 py-1 mr-1 rounded-full border border-navy text-white bg-cyan-900 text-navy hover:bg-cyan-700">
-          リスニング機能
-        </Button>
-      </Tooltip>
+      {tooltipData.map((data, index) => (
+        <div
+          key={index}
+          className="tooltip tooltip-bottom tooltip-accent mr-2"
+          data-tip={data.explain}
+        >
+          <button className="btn btn-info">{data.btnText}</button>
+        </div>
+      ))}
     </div>
   );
 };
