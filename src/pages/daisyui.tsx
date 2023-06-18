526c1bbb303885b8eeff06e3df8844dbeb0869d3
hori <fourbasic19@gmail.com>
2023-06-18 18:54:00 +0900

    【daisyui】アコーディオン作成

--- src/pages/daisyui.tsx
+++ src/pages/daisyui.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineDownCircle } from "react-icons/ai";
 
-const daisyui = () => {
+const DaisyUI = () => {
   return (
     <div className="my-4 mx-2">
-      <button className="btn">Hello daisyUI</button>
+      <button className="btn mb-2">Hello daisyUI</button>
+      <div className="collapse bg-base-200 mb-2 text-white">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-lime-600 [input:checked~&]:bg-lime-500 border border-b-0 border-gray-100">
+          Click me to show/hide content
+          <span className="absolute right-4 transform transition-transform">
+            <AiOutlineDownCircle
+              className="icon text-white inline-block"
+              size="1.7rem"
+            />
+          </span>
+        </div>
+        <div className="collapse-content  bg-orange-500 border border-t-0 border-gray-300">
+          <p className="pt-2">hello</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default daisyui
+export default DaisyUI;
