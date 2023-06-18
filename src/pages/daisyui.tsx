28576011ba8e3bb66f34bd90a3873ddb05ef78db
hori <fourbasic19@gmail.com>
2023-06-18 18:16:31 +0900

    【daisyui】Button

---
+++ src/pages/daisyui.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const daisyui = () => {
+  return (
+    <div className="my-4 mx-2">
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default daisyui
