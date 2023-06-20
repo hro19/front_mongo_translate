37c22292cf5f0353ee2d1f7f84b5d80196d16d3b
hori <fourbasic19@gmail.com>
2023-06-20 14:01:15 +0900

    【jotai】タブの切り替えのselect要素をjotaiに記述

--- src/components/tasks/TaskSelect.tsx
+++ src/components/tasks/TaskSelect.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { useAtom } from "jotai";
-import { tasksStateAtom } from "../../jotai/atoms";
+import { tasksStateAtom, taskTabAtom } from "../../jotai/atoms";
 import { SelectSwitch } from "../../ts/Task";
 
 const TaskSelect = () => {
+  const [taskTab] = useAtom(taskTabAtom);
   const [tasksState, setTasksState] = useAtom(tasksStateAtom);
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value as SelectSwitch;
-    setTasksState(value);
-    };
-
-    const selectSwitch: Record<SelectSwitch, string> = {
-      uncompleted: "暗記中",
-      completed: "暗記済み",
-      all: "全て",
+      const value = e.target.value as SelectSwitch;
+      setTasksState(value);
     };
     
   return (
@@ -25,7 +20,7 @@ const TaskSelect = () => {
         onChange={handleChange}
         className="bg-yellow-600 text-white py-2 pl-2 pr-4 rounded-lg cursor-pointer"
       >
-        {Object.entries(selectSwitch).map(([value, label]) => (
+        {Object.entries(taskTab).map(([value, label]) => (
           <option key={value} value={value}>
             {label}
           </option>
--- src/jotai/atoms.tsx
+++ src/jotai/atoms.tsx
@@ -4,10 +4,19 @@ import { SelectSwitch, Task } from "../ts/Task";
 // tasks一覧ページ
 export const tasksStateAtom = atom<SelectSwitch>("all");
 
+//タブの切り替えのselect要素
+const taskTab: Record<SelectSwitch, string> = {
+  uncompleted: "勉強中",
+  completed: "暗記済み",
+  all: "全て",
+};
+export const taskTabAtom = atom(taskTab);
+
 // tasksシングルページ
 export const isSnakeAtom = atom<boolean>(false);
 export const snakeDurationAtom = atom<number>(2000);
 export const checkEditAtom = atom<boolean>(false);
+
 
 export const nameAtom = atom<string>("");
 export const jaNameAtom = atom<string>("");
@@ -32,9 +41,9 @@ const initialSpeechOptions: Record<string, string> = {
   auxiliaryVerb: "助動詞",
   gerund: "動名詞",
 };
-
 export const initialSpeechOptionsAtom = atom(initialSpeechOptions);
 
+//品詞のkeyから日本語の文字
 export const getSpeechLabel = (speech: keyof typeof initialSpeechOptions) => {
   return initialSpeechOptions[speech] || "";
 };
