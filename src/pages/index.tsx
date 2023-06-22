8c6b2f5c341952d56f57d46d6bde7c47f3e90a07
hori <fourbasic19@gmail.com>
2023-06-22 21:10:14 +0900

    【バグフィックス】next/linkを使用してリンク

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { AiOutlineDownCircle } from "react-icons/ai";
+import Link from "next/link";
 
 const DaisyUI = () => {
   return (
     <div className="my-4 mx-2">
-        <a href="/tasks/exam" className="btn mb-2 bg-primary text-white">
-          英単語テスト
-        </a>
-        <a href="/tasks" className="btn mb-2 bg-secondary text-white">
-          英単語登録
-        </a>
-        <a href="/translates" className="btn mb-2 bg-accent-focus text-white">
-          英文暗記帳
-        </a>
+      <Link href="/tasks/exam" className="btn mb-2 bg-primary text-white">英単語テスト</Link>
+      <Link href="/tasks" className="btn mb-2 bg-secondary text-white">英単語登録</Link>
+      <Link href="/translates" className="btn mb-2 bg-accent-focus text-white">英文暗記帳</Link>
     </div>
   );
 };
