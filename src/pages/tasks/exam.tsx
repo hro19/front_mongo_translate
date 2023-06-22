6818a882ddef5d13eca640c616c1d6d8cdb441b6
hori <fourbasic19@gmail.com>
2023-06-22 19:28:02 +0900

    【react】試験ページの設定

---
+++ src/pages/tasks/exam.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const exam = () => {
+  return (
+    <div>
+          <h2>{ }の意味は</h2>
+          <ul>
+            <li>aaaa</li>
+            <li>nnnn</li>
+            <li>eeee</li>
+          </ul>
+    </div>
+  )
+}
+
+export default exam
