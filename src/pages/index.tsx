3d7fab7c9ad6816b1a8d78c07fd4721907ea9b71
hori <fourbasic19@gmail.com>
2023-06-22 17:31:39 +0900

    【daisyUI】トップページを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from "react";
+import { AiOutlineDownCircle } from "react-icons/ai";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const DaisyUI = () => {
   return (
-    <main
-      className="text-amber-700"
-    >表示成功</main>
-  )
-}
+    <div className="my-4 mx-2">
+        <a href="/tasks/exam" className="btn mb-2 bg-primary text-white">
+          英単語テスト
+        </a>
+        <a href="/tasks" className="btn mb-2 bg-secondary text-white">
+          英単語登録
+        </a>
+        <a href="/translates" className="btn mb-2 bg-accent-focus text-white">
+          英文暗記帳
+        </a>
+    </div>
+  );
+};
+
+export default DaisyUI;
