86021910cd0de9afb79899d5c90a88d3746db99e
hori <fourbasic19@gmail.com>
2023-06-25 17:56:04 +0900

    【react】問題形式ベーシック

--- src/pages/tasks/exam.tsx
+++ src/pages/tasks/exam.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const exam = () => {
+const Exam = () => {
+    const [answers, setAnswers] = useState<string[]>([]);
+    type Gamen = "default" | "question" | "answer" | "finish";
+    const [gamen, setgamen] = useState<Gamen>("default");
+
+  const changehandle = (value: string) => {
+    setAnswers((prevAnswers) => [...prevAnswers, value]);
+  };
+
   return (
     <div>
-          <h2>{ }の意味は</h2>
-          <ul>
-            <li>aaaa</li>
-            <li>nnnn</li>
-            <li>eeee</li>
-          </ul>
+      <div>
+        <h2>{}の意味は</h2>
+        <ul>
+          <li>
+            <button onClick={() => changehandle("aaaa")}>aaaa</button>
+          </li>
+          <li>
+            <button onClick={() => changehandle("nnnn")}>nnnn</button>
+          </li>
+          <li>
+            <button onClick={() => changehandle("eeee")}>eeee</button>
+          </li>
+        </ul>
+        {answers.map(answer=>answer)}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default exam
+export default Exam;
