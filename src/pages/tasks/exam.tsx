2fb7400e1e5bef2a4c24515c1ecd69482f76ffd1
hori <fourbasic19@gmail.com>
2023-06-25 18:18:17 +0900

    【tailwind】ボタンのcssを調整

--- src/pages/tasks/exam.tsx
+++ src/pages/tasks/exam.tsx
@@ -1,30 +1,49 @@
 import React, { useState } from "react";
 
 const Exam = () => {
-    const [answers, setAnswers] = useState<string[]>([]);
-    type Gamen = "default" | "question" | "answer" | "finish";
-    const [gamen, setgamen] = useState<Gamen>("default");
+  const [answers, setAnswers] = useState<string[]>([]);
+  type Gamen = "default" | "question" | "answer" | "finish";
+  const [gamen, setGamen] = useState<Gamen>("default");
 
-  const changehandle = (value: string) => {
+  const changeHandle = (value: string) => {
     setAnswers((prevAnswers) => [...prevAnswers, value]);
+    setGamen("answer");
   };
 
   return (
     <div>
-      <div>
+      <div className="mx-auto max-w-[640px]">
         <h2>{}の意味は</h2>
-        <ul>
+        <ul className="flex flex-col justify-center">
           <li>
-            <button onClick={() => changehandle("aaaa")}>aaaa</button>
+            <button
+              className="btn mt-9 mb-2 w-full bg-primary text-white py-2 px-4 text-lg"
+              onClick={() => changeHandle("aaaa")}
+            >
+              aaaa
+            </button>
           </li>
           <li>
-            <button onClick={() => changehandle("nnnn")}>nnnn</button>
+            <button
+              className="btn mt-9 mb-2 w-full bg-primary text-white py-2 px-4 text-lg"
+              onClick={() => changeHandle("nnnn")}
+            >
+              nnnn
+            </button>
           </li>
           <li>
-            <button onClick={() => changehandle("eeee")}>eeee</button>
+            <button
+              className="btn mt-9 mb-2 w-full bg-primary text-white py-2 px-4 text-lg"
+              onClick={() => changeHandle("eeee")}
+            >
+              eeee
+            </button>
           </li>
         </ul>
-        {answers.map(answer=>answer)}
+        {answers.map((answer, index) => (
+          <p key={index}>{answer}</p>
+        ))}
+        {gamen}
       </div>
     </div>
   );
