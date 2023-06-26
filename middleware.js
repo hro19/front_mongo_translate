4ee7f49695d19e17920b978e18914a1945527b6a
hori <fourbasic19@gmail.com>
2023-06-26 13:49:25 +0900

    【basic認証】

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+/*
+matcher : middleware.jsを適用する（呼び出す）パスを指定する
+*/
+export const config = {
+    matcher: ['/:path*', '/tasks/:path*'],
+}
+
+export function middleware(req) {
+    const basicAuth = req.headers.get('authorization')
+    const url = req.nextUrl
+
+    if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1]
+	const [user, pwd] = Buffer.from(authValue, 'base64').toString().split(':');
+
+        if (user === "test" && pwd === "test") {
+            return NextResponse.next()
+        }
+    }
+    url.pathname = '/api/auth'
+
+    return NextResponse.rewrite(url)
+}
