649ec697e9905ef9216aa448772e6055accc3e90
hori <fourbasic19@gmail.com>
2023-06-30 20:16:37 +0900

    【canvas-confetti】紙吹雪や量を調整

--- src/components/exam/Fubuki.tsx
+++ src/components/exam/Fubuki.tsx
@@ -3,22 +3,26 @@ import confetti from "canvas-confetti";
 
 const ConfettiBackground = () => {
   useEffect(() => {
-    const interval = setInterval(() => {
+    // 紙吹雪を生成する関数
+    function createConfetti() {
       // 紙吹雪の設定
       const options = {
-        particleCount: 100, // 紙吹雪の数
+        particleCount: 30, // 紙吹雪の数
         spread: 160, // 紙吹雪が広がる範囲
-        origin: { y: 0.6 }, // 紙吹雪が発生する位置（上から60%の位置）
+        origin: { y: 0.4 }, // 紙吹雪が発生する位置（上から60%の位置）
       };
 
       // 紙吹雪を発生させる
       confetti(options);
-    }, 200);
+    }
 
-    return () => {
-      // コンポーネントがアンマウントされた時にクリア
-      clearInterval(interval);
-    };
+    // 紙吹雪を生成
+    createConfetti();
+
+    // 1秒後に紙吹雪を停止
+    setTimeout(() => {
+      confetti.reset(); // 紙吹雪をリセットして停止
+    }, 1000);
   }, []);
 
   return <div id="confetti-container" />;
