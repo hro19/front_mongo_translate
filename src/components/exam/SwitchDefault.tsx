3874adfafa2052fa7a3ab57064de17c0c8145a26
hori <fourbasic19@gmail.com>
2023-07-01 18:31:24 +0900

    【tailwind】cssにより調整

--- src/components/exam/SwitchDefault.tsx
+++ src/components/exam/SwitchDefault.tsx
@@ -11,8 +11,17 @@ const SwitchDefault = () => {
 
   return (
     <div className="my-8">
-      <button onClick={handleButtonClick} className="btn btn-info">英単語テストをスタート</button>
-      <div>デフォルト画面</div>
+      <div className="mb-4">
+        <h2 className="text-2xl font-bold">ルール説明</h2>
+        <ul className="list-disc ml-6">
+          <li>答えが分からないときは【Enter】キーで次の問題に移動します</li>
+          <li>最後に間違えた一覧を表示しますので、復習ができます</li>
+          <li>...</li>
+        </ul>
+      </div>
+      <button onClick={handleButtonClick} className="btn btn-info">
+        英単語テストをスタート
+      </button>
     </div>
   );
 };
