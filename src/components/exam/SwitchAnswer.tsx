620e0895b7b84ae026a23c9641761a47601548b3
hori <fourbasic19@gmail.com>
2023-07-03 23:05:23 +0900

    【バグフィックス】reactのインポートが抜けておりました

--- src/components/exam/SwitchAnswer.tsx
+++ src/components/exam/SwitchAnswer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { CandidatesTask } from "@/ts/Task";
+import { CandidatesTask } from "../../ts/Task";
 import { useAtom } from "jotai";
 import {
   HOWManyLesson,
