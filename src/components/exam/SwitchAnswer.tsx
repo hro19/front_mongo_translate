d07f408a1cd80e547a737f050b58311eba1a2649
hori <fourbasic19@gmail.com>
2023-07-03 18:27:40 +0900

    【react】nextQuizHandleをSwithAnswerに移動

--- src/components/exam/SwitchAnswer.tsx
+++ src/components/exam/SwitchAnswer.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { CandidatesTask } from "@/ts/Task";
+import { useAtom } from "jotai";
+import {
+  HOWManyLesson,
+  failuresAtom,
+  gamenAtom,
+  quizListCountAtom,
+  isJadgeAtom,
+} from "../../jotai/atoms";
 
-const SwitchAnswer = ({ onClick }: any) => {
+const SwitchAnswer = ({ currentQuizData }: { currentQuizData: CandidatesTask }) => {
+  const [failures, setFailures] = useAtom(failuresAtom);
+  const [gamen, setGamen] = useAtom(gamenAtom);
+  const [quizListCount, setQuizListCount] = useAtom(quizListCountAtom);
+  const [isJadge, setIsJadge] = useAtom(isJadgeAtom);
+
+  //次の問題へボタンまたはエンターkeyを押したときの反応
+  const nextQuizHandle = (currentQuizData: CandidatesTask) => {
+    setQuizListCount((prevCount) => prevCount + 1);
+
+    if (isJadge === null || isJadge === false) {
+      setFailures((prevFailures) => [...prevFailures, currentQuizData]);
+    }
+    setIsJadge(null);
+
+    // setQuizListCountを使うと非同期で計算が遅れるためにquizListCountを利用して計算する。
+    quizListCount + 1 === HOWManyLesson ? setGamen("finish") : setGamen("question");
+  };
+
+  //エンターkeyを押したときに次の問題に行くためのトリガー設定
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        nextQuizHandle(currentQuizData);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [quizListCount, isJadge]);
 
   return (
     <div className="flex justify-end">
-      <button
-        onClick={onClick}
-        className="btn btn-outline btn-primary mt-4 mb-4 justify-end"
-      >
-        次の問題へ
-      </button>
+      {gamen === "answer" && (
+        <button
+          onClick={() => nextQuizHandle(currentQuizData)}
+          className="btn btn-outline btn-primary mt-4 mb-4 justify-end"
+        >
+          次の問題へ
+        </button>
+      )}
     </div>
   );
 };
--- src/components/exam/SwitchQanda.tsx
+++ src/components/exam/SwitchQanda.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import { CandidatesTask, JadgeTask } from "@/ts/Task";
 import { useAtom } from "jotai";
 import {
-  HOWManyLesson,
-  failuresAtom,
   gamenAtom,
   quizListCountAtom,
   isJadgeAtom,
@@ -14,7 +12,6 @@ import TimeCount from "../../components/exam/TimeCount";
 import SwithchAnswer from "../../components/exam/SwitchAnswer";
 
 const SwitchQanda = ({ quizListData }: { quizListData: CandidatesTask[] }) => {
-  const [failures, setFailures] = useAtom(failuresAtom);
   const [gamen, setGamen] = useAtom(gamenAtom);
   const [quizListCount, setQuizListCount] = useAtom(quizListCountAtom);
   const [isJadge, setIsJadge] = useAtom(isJadgeAtom);
@@ -24,33 +21,6 @@ const SwitchQanda = ({ quizListData }: { quizListData: CandidatesTask[] }) => {
     setIsJadge(name === currentQuizData.name ? true : false);
     setGamen("answer");
   };
-
-  //次の問題へボタンまたはエンターkeyを押したときの反応
-  const nextQuizHandle = (currentQuizData: CandidatesTask) => {
-    setQuizListCount((prevCount) => prevCount + 1);
-
-    if (isJadge === null || isJadge === false) {
-      setFailures((prevFailures) => [...prevFailures, currentQuizData]);
-    }
-    setIsJadge(null);
-
-    // setQuizListCountを使うと非同期で計算が遅れるためにquizListCountを利用して計算する。
-    quizListCount + 1 === HOWManyLesson ? setGamen("finish") : setGamen("question");
-  };
-
-  //エンターkeyを押したときに次の問題に行くためのトリガー設定
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Enter") {
-        nextQuizHandle(quizListData[quizListCount]);
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [quizListCount, isJadge]);
 
   return (
     <div>
@@ -80,9 +50,7 @@ const SwitchQanda = ({ quizListData }: { quizListData: CandidatesTask[] }) => {
         </div>
       )}
       {gamen === "answer" && <MaruBatsu />}
-      {gamen === "answer" && (
-        <SwithchAnswer onClick={() => nextQuizHandle(quizListData[quizListCount])} />
-      )}
+      <SwithchAnswer currentQuizData={quizListData[quizListCount]} />
     </div>
   );
 };
