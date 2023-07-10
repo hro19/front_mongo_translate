635f18bbc09a5ca8284adb9902c117f35db56e51
hori <fourbasic19@gmail.com>
2023-07-10 23:07:58 +0900

    【バグフィックス】examsが値を持っていないときのために条件分岐を記述

--- src/pages/ExamsChart.tsx
+++ src/pages/ExamsChart.tsx
@@ -13,41 +13,44 @@ import {
 import { ExamsWithRate } from "../ts/Exam";
 
 const ExamsChart = ({ exams }: { exams: ExamsWithRate[] }) => {
+  if (!exams || exams.length === 0) {
+    return <div>No data available</div>;
+  }
 
   const data = exams.map((exam: ExamsWithRate) => ({
     date: format(new Date(exam.created_at), "yy年M月d日"),
     正解率: exam.dailyRate, // dailyRateを正解率として使用
   }));
 
-    return (
-      <div className="my-8 pb-8">
-        <div className="container mx-auto" style={{ width: "100%", height: 300 }}>
-          <ResponsiveContainer>
-            <LineChart
-              data={data}
-              margin={{
-                top: 5,
-                right: 45,
-                left: 5,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="date" />
-              <YAxis domain={[0, 100]} />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="正解率"
-                strokeWidth={2} // 線の太さを調整
-                stroke="#34A853" // 線の色を緑色に設定
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+  return (
+    <div className="my-8 pb-8">
+      <div className="container mx-auto" style={{ width: "100%", height: 300 }}>
+        <ResponsiveContainer>
+          <LineChart
+            data={data}
+            margin={{
+              top: 5,
+              right: 45,
+              left: 5,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis domain={[0, 100]} />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="正解率"
+              strokeWidth={2} // 線の太さを調整
+              stroke="#34A853" // 線の色を緑色に設定
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ExamsChart;
