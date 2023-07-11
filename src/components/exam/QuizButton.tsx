bd48cb14040eae39f1c0ac80d252b90802bbf83c
hori <fourbasic19@gmail.com>
2023-07-12 03:50:15 +0900

    【daisyUI】選択問題をhover時に少しだけ背景色を濃くする

--- src/components/exam/QuizButton.tsx
+++ src/components/exam/QuizButton.tsx
@@ -14,7 +14,7 @@ const QuizButton = ({ onClick, candidate }: QuizButtonProps) => {
   return (
     <li>
       <button
-        className={`btn normal-case mt-9 mb-2 w-full text-white py-2 px-4 text-lg bg-primary ${
+        className={`btn normal-case mt-9 mb-2 w-full text-white py-2 px-4 text-lg bg-primary hover:bg-primary-focus ${
           gamen === "answer" && candidate.correct
             ? "disabled:bg-accent disabled:text-white"
             : gamen === "answer" && !candidate.correct
