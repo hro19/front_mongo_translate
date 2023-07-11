2cf5d4036b7ad516e53d47a2d30d646ffba3399b
hori <fourbasic19@gmail.com>
2023-07-12 02:58:58 +0900

    【Ts】各関数に型指定する

--- src/pages/idExams.tsx
+++ src/pages/idExams.tsx
@@ -8,17 +8,13 @@ import { Exam, ExamsWithRate, Chart } from "../ts/Exam";
 const IdExams = () => {
   const results = [
     { id: "649ee23af35c07578a186814", name: "fear", jaName: "恐怖する" },
-    {
-      id: "648f04e7d01e7ef14aea4e8f",
-      name: "dispatch",
-      jaName: "(…へ)発送する、特派する",
-    },
+    { id: "648f04e7d01e7ef14aea4e8f", name: "dispatch", jaName: "(…へ)発送する、特派する"},
     { id: "648280313e790e70e4646db4", name: "fall", jaName: "落ちる、転ぶ" },
     { id: "649aa99815ca83cdb2ccba0b", name: "rise", jaName: "上がる" },
     { id: "64a41b8fb1ec37f082321984", name: "meet", jaName: "会う" },
   ];
 
-  const taskIdFetching = async (id: string) => {
+  const taskIdFetching = async (id: string): Promise<Exam[]> => {
     const response = await axios.get(
       `https://back-mongo-task2.vercel.app/api/v1/tasks/${id}/exams`
     );
@@ -38,35 +34,37 @@ const IdExams = () => {
     });
   };
 
+  //チャート用のデータオブジェクトの配列をmapにて作成
+  const promises: Promise<Chart>[] = results.map(async (result) => {
+    const exams: Exam[] = await taskIdFetching(result.id);
+
+    // テストの回数を算出
+    const totalCount: number = exams.length;
+
+    // 正解回数を算出
+    const totalCorrectCount: number = exams.filter((exam: Exam) => exam.isCorrect).length;
+
+    // 正答率を算出
+    const correctRate: number = (totalCorrectCount / totalCount) * 100;
+
+    // examsWithRateはexamsとdailyRateを合わせたもの
+    const examsWithRates: ExamsWithRate[] = addDailyRateToExams(exams);
+
+    return {
+      taskId: result.id,
+      name: result.name,
+      jaName: result.jaName,
+      totalCount,
+      totalCorrectCount,
+      correctRate,
+      examsWithRates,
+    };
+  });
+
+  //フェッチング時のキャッシュ管理とローディング管理
   const { data: charts, isLoading } = useQuery("charts", async () => {
-    const promises: Promise<Chart>[] = results.map(async (result) => {
-      const exams = await taskIdFetching(result.id);
-
-      // テストの回数を算出
-      const totalCount = exams.length;
-
-      // 正解回数を算出
-      const totalCorrectCount = exams.filter((exam: Exam) => exam.isCorrect).length;
-
-      // 正答率を算出
-      const correctRate = (totalCorrectCount / totalCount) * 100;
-
-      // examsWithRateはexamsとdailyRateを合わせたもの
-      const examsWithRates: ExamsWithRate[] = addDailyRateToExams(exams);
-
-      return {
-        taskId: result.id,
-        name: result.name,
-        jaName: result.jaName,
-        totalCount,
-        totalCorrectCount,
-        correctRate,
-        examsWithRates,
-      };
-    });
-
-    const taskExams = await Promise.all(promises);
-    return taskExams;
+    const charts: Chart[] = await Promise.all(promises);
+    return charts;
   });
 
   if (isLoading) {
@@ -80,17 +78,18 @@ const IdExams = () => {
   return (
     <div className="container mx-auto">
       <div className="my-8">
-        {charts && charts.map((chart: Chart) => (
-          <div key={chart.taskId}>
-            <h2 className="text-2xl text-sky-700">【英単語】{chart.name}</h2>
-            <div className="flex flex-row text-sm text-sky-900 gap-6">
-              <p>【最新の正答率】 {chart.correctRate.toFixed(2)}%</p>
-              <p>試験回数:{chart.totalCount}</p>
-              <p>試験の正解数:{chart.totalCorrectCount}</p>
+        {charts &&
+          charts.map((chart: Chart) => (
+            <div key={chart.taskId}>
+              <h2 className="text-2xl text-sky-700">【{ chart.name }】{ chart.jaName }</h2>
+              <div className="flex flex-row text-sm text-sky-900 gap-6">
+                <p>【最新の正答率】 {chart.correctRate.toFixed(2)}%</p>
+                <p>試験回数:{chart.totalCount}</p>
+                <p>試験の正解数:{chart.totalCorrectCount}</p>
+              </div>
+              <ExamsChart exams={chart.examsWithRates} />
             </div>
-            <ExamsChart exams={chart.examsWithRates} />
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
