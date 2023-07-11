80f49a172e11423cca5dc7674029391e521e2906
hori <fourbasic19@gmail.com>
2023-07-12 00:49:32 +0900

    【jotai】セッター関数にRESETを引数として、値を初期化する

--- src/pages/JotaiReset.tsx
+++ src/pages/JotaiReset.tsx
@@ -1,8 +1,8 @@
 import { atom, useAtom, useAtomValue } from "jotai";
-import { atomWithReset, useResetAtom } from "jotai/utils";
+import { atomWithReset, useResetAtom,RESET } from "jotai/utils";
 
 const count1Atom = atom(1);
-const count2Atom = atomWithReset(10);
+const count2Atom = atomWithReset<number>(10);
 
 const Counter = () => {
   const [count2, setCount2] = useAtom(count2Atom);
@@ -12,11 +12,14 @@ const Counter = () => {
     <div>
       <div>count1: {useAtomValue(count1Atom)}</div>
       <div>count2: {count2}</div>
-      <button className="btn btn-primary" onClick={() => setCount2((c) => c + 1)}>
+      <button className="btn btn-primary mr-3" onClick={() => setCount2((c) => c + 1)}>
         +1
       </button>
-      <button className="btn btn-secondary" onClick={resetCount2}>
+      <button className="btn btn-secondary mr-3" onClick={resetCount2}>
         reset
+      </button>
+      <button className="btn btn-accent mr-3" onClick={() => setCount2(RESET)}>
+        reset2
       </button>
     </div>
   );
