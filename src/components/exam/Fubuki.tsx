ab8de9d34114494d7faab7df641611c41433f0f3
hori <fourbasic19@gmail.com>
2023-07-13 18:19:28 +0900

    【canvas-confetti】関数化をする

--- src/components/exam/Fubuki.tsx
+++ src/components/exam/Fubuki.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from "react";
 import confetti from "canvas-confetti";
 
+// 紙吹雪を生成する関数
+function createConfetti() {
+  // 紙吹雪の設定
+  const options = {
+    particleCount: 900, // 紙吹雪の数
+    angle: 90, // 紙吹雪の放射方向の角度
+    spread: 260, // 紙吹雪が広がる範囲
+    origin: { y: 0.75 }, // 紙吹雪が発生する位置（上から60%の位置）
+  };
+
+  // 紙吹雪を発生させる
+  confetti(options);
+}
+
 const ConfettiBackground = () => {
   useEffect(() => {
-    // 紙吹雪を生成する関数
-    function createConfetti() {
-      // 紙吹雪の設定
-      const options = {
-        particleCount: 900, // 紙吹雪の数
-        angle: 90, // 紙吹雪の放射方向の角度
-        spread: 260, // 紙吹雪が広がる範囲
-        origin: { y: 0.75 }, // 紙吹雪が発生する位置（上から60%の位置）
-      };
-
-      // 紙吹雪を発生させる
-      confetti(options);
-    }
-
     // 紙吹雪を生成
     createConfetti();
 
