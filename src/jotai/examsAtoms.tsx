b1c16f4dfb13ae3d171e6915e8f0d5116bef0638
hori <fourbasic19@gmail.com>
2023-07-17 22:08:01 +0900

    【Ts】satisfiesを使って、定数を書き替える。

--- src/jotai/examsAtoms.tsx
+++ src/jotai/examsAtoms.tsx
@@ -4,9 +4,9 @@ import { CandidatesTask } from "../ts/Task";
 import { Result, Gamen, Chart } from "../ts/Exam";
 
 // 単語テストページ
-export const HOWManyLesson: number = 5; //問題数
-export const HOWManySelect: number = 4; //問題時の選択肢の数
-export const countdownTime: number = 4; //回答中の残り時間
+export const HOWManyLesson = 5 as const satisfies number; //問題数
+export const HOWManySelect = 4 as const satisfies number; //問題時の選択肢の数
+export const countdownTime = 4 as const satisfies number; //回答中の残り時間
 
 // export const failuresAtom = atom<CandidatesTask[]>([]);
 
