c17f3047be3fd42652a19235abc2dc394f28d04a
hori <fourbasic19@gmail.com>
2023-07-24 19:01:39 +0900

    【js】全サウンド再生機能追記

--- src/components/tasks/TaskAllSounds.tsx
+++ src/components/tasks/TaskAllSounds.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import { Task } from "../../ts/Task";
 import { speakText } from "../translates/Onsei";
 
+const speakEnJa = (task: Task) => {
+  const utterance = new SpeechSynthesisUtterance(task.name);
+  utterance.lang = "en-US";
+
+  speechSynthesis.speak(utterance);
+
+  // onendイベントにコールバック関数を登録する
+  utterance.onend = () => {
+    speakText(task.jaName,"ja");
+    // console.log("音声再生が終了しました！"); // 音声再生が終了した後に実行されます
+  };
+
+  return utterance;
+};
+
 const TaskAllSounds = ({ tasks }: { tasks: Task[] }) => {
   return (
     <div>
-      <button onClick={() => speakText(tasks[0].jaName,"ja")} className="btn btn-secondary">
-        全サウンド再生{tasks[0].name}
+      <button
+        onClick={() => tasks.map(task=>speakEnJa(task))}
+        className="btn btn-secondary"
+      >
+        全サウンド再生
       </button>
     </div>
   );
