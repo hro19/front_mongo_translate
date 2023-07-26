afaec949f28b16ff5f6e1af01eb014972a0775b1
hori <fourbasic19@gmail.com>
2023-07-27 02:08:27 +0900

    【SpeechSynthesisUtterance】英単語と日本単語、また各taskの間隔にms秒遅延をする。遅延中も音声ストップできるようにwindow.speechSynthesis.pendingを追記

--- src/components/tasks/TaskAllSounds.tsx
+++ src/components/tasks/TaskAllSounds.tsx
@@ -1,34 +1,61 @@
 import React, { useEffect } from "react";
 import { Task } from "../../ts/Task";
 import { useAtom } from "jotai";
-import {filteredTasksAtom} from "../../jotai/atoms";
+import { filteredTasksAtom } from "../../jotai/atoms";
 
 const TaskAllSounds = () => {
   const [filteredTasks] = useAtom(filteredTasksAtom);
 
   const speakAllTasks = async (filteredTasks: Task[]) => {
+    let firstFlag = true;
     for (const task of filteredTasks) {
       const utteranceEn = new SpeechSynthesisUtterance(task.name);
       const utteranceJa = new SpeechSynthesisUtterance(task.jaName);
       utteranceEn.lang = "en-US";
       utteranceJa.lang = "ja-JP";
 
-      speechSynthesis.speak(utteranceEn);
-      speechSynthesis.speak(utteranceJa);
+      if (firstFlag) {
+        await speakAsync(utteranceEn);
+        firstFlag = false;
+      } else {
+        await speakAsyncWithDelay(utteranceEn, 4000);
+      }
+      await speakAsyncWithDelay(utteranceJa, 800);
     }
   };
-  
+
+  const speakAsync = (utterance: SpeechSynthesisUtterance) => {
+    return new Promise((resolve, reject) => {
+      utterance.onend = resolve;
+      utterance.onerror = reject;
+      speechSynthesis.speak(utterance);
+    });
+  };
+
+  const speakAsyncWithDelay = (utterance: SpeechSynthesisUtterance, delay: number) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(speakAsync(utterance));
+      }, delay);
+    });
+  };
 
   //音声出力中に音声ストップ
   const stopSpeaking = () => {
-    if ("speechSynthesis" in window && window.speechSynthesis.speaking) {
+    if (
+      ("speechSynthesis" in window && window.speechSynthesis.speaking) ||
+      window.speechSynthesis.pending
+    ) {
       window.speechSynthesis.cancel();
     }
   };
 
   return (
     <div>
-      <button onClick={() => speakAllTasks(filteredTasks)} className="btn btn-secondary mr-2">
+      <button
+        onClick={() => speakAllTasks(filteredTasks)}
+        className="btn btn-secondary mr-2"
+      >
         全サウンド再生
       </button>
       <button onClick={() => stopSpeaking()} className="btn btn-error">
