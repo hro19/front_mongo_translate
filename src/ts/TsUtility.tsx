dc97949f5e72cf0501ca28624a8438377bb0d62a
hori <fourbasic19@gmail.com>
2023-07-28 16:44:39 +0900

    【Ts】UtilityにValueOfを新設

--- src/ts/TsUtility.tsx
+++ src/ts/TsUtility.tsx
@@ -2,6 +2,11 @@
 //条件分岐次第で型が変わる。
 //第一引数 true/false
 //第二引数 プリミティブ値またはリテラル値
-//第一引数 プリミティブ値またはリテラル値
+//第三引数 プリミティブ値またはリテラル値
 
 export type If<C extends boolean, T, F> = C extends true ? T : F;
+
+
+//オブジェクトのvalueのユニオン型を作る
+//第一引数 オブジェクト
+export type valueOf<T> = T[keyof T];
