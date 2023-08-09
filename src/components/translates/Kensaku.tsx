64371a2afb524fb45eb403d2b81fc4dc89ad9f32
hori <fourbasic19@gmail.com>
2023-08-09 14:42:05 +0900

    【Ts】関数searchFuncに型をエイリアスを作成し、定義

--- src/components/translates/Kensaku.tsx
+++ src/components/translates/Kensaku.tsx
@@ -1,7 +1,14 @@
 import { Translate } from "../../ts/Translate";
 
+type SearchFuncType = (params: {
+  value: string;
+  posts: Translate[];
+  setShowPosts: React.Dispatch<React.SetStateAction<Translate[]>>;
+}) => void;
+
+
 //検索文字に対応したコンテンツをフィルター
-const searchFunc = ({ value, posts, setShowPosts }: any) => {
+const searchFunc: SearchFuncType = ({ value, posts, setShowPosts }) => {
   // postsが存在する場合はフィルタリングを行う
   if (posts) {
     // 検索欄への入力が空の場合は全ての投稿を表示する
