76eed5776400e7278bc4f34134c309c536d29d91
hori <fourbasic19@gmail.com>
2023-11-06 17:16:40 +0900

    【ts-pattern】パターンマッチング

--- src/pages/exam/index.tsx
+++ src/pages/exam/index.tsx
@@ -7,6 +7,7 @@ import SwitchFinish from "../../components/exam/SwitchFinish";
 import Densya from "../../components/exam/Densya";
 import { getAllTasks } from "../../api/task";
 import IdExams from "@/components/exam/IdExams";
+import { match } from "ts-pattern";
 
 const Exam = ({ data }: { data: Task[] }) => {
   const [gamen] = useAtom(gamenAtom);
@@ -14,19 +15,11 @@ const Exam = ({ data }: { data: Task[] }) => {
   return (
     <>
       <div className="mx-auto max-w-[640px]">
-        {(() => {
-          switch (gamen) {
-            case "default":
-              return <SwitchDefault data={data} />;
-            case "answer":
-            case "question":
-              return <SwitchQanda />;
-            case "finish":
-              return <SwitchFinish />;
-            default:
-              return null;
-          }
-        })()}
+        {match(gamen)
+          .with("default", () => <SwitchDefault data={data} />)
+          .with("answer", "question", () => <SwitchQanda />)
+          .with("finish", () => <SwitchFinish />)
+          .otherwise(() => null)}
       </div>
       <Densya />
       {gamen === "finish" && <IdExams />}
