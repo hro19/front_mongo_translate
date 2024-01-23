d2ba91ca62433dce229476512aba2f8d7f43cef8
hori <fourbasic19@gmail.com>
2024-01-23 14:52:52 +0900

    【daisy,tailwind】ルールページcss調整

--- src/components/exam/SwitchDefault.tsx
+++ src/components/exam/SwitchDefault.tsx
@@ -35,16 +35,41 @@ const SwitchDefault = ({ data }: { data: Task[] }) => {
   return (
     <div className="">
       <div className="mb-4">
-        <h2 className="text-2xl font-bold">ルール説明</h2>
-        <ul className="list-disc ml-6">
+        <h1 className="mb-8 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl">
+          単語クイズの
+          <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">
+            ルール説明
+          </span>
+        </h1>
+        <h2 className="text-2xl font-bold"></h2>
+        <ul className="list-disc ml-6 space-y-3 text-slate-600">
+          <li>
+            各品詞に合わせて単語クイズを実践できます。（動詞以外は順次オープンする予定です）
+          </li>
+          <li>
+            各問題の制限時間は5秒しかありませんので、集中力があるときにするのがおすすめです
+          </li>
           <li>答えが分からないときは【Enter】キーで次の問題に移動します</li>
           <li>最後に間違えた一覧を表示しますので、復習ができます</li>
-          <li>...</li>
+          <li>
+            該当単語の今までの正解率をグラフに表示するので自身の苦手なワードを確認してください
+          </li>
         </ul>
       </div>
-      <button onClick={() => startBtn("verb")} className="btn btn-info">
-        【動詞単語】テストをスタート
-      </button>
+      <div className="flex flex-wrap gap-2 pt-8">
+        <button onClick={() => startBtn("verb")} className="btn btn-info">
+          【動詞単語】テスト
+        </button>
+        <a href="#" className="btn btn-active btn-neutral pointer-events-none">
+          【名詞単語】テスト(準備中)
+        </a>
+        <a href="#" className="btn btn-active btn-neutral pointer-events-none">
+          【形容詞単語】テスト(準備中)
+        </a>
+        <a href="#" className="btn btn-active btn-neutral pointer-events-none">
+          【副詞単語】テスト(準備中)
+        </a>
+      </div>
       <QuizSetting />
     </div>
   );
